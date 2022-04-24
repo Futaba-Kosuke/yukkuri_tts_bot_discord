2e76f316446c6ace3c738ba12cd060e7ef4695d3
Futaba-Kosuke <k.futaba2828@gmail.com>
2022-04-24 17:40:00 +0900

    Update: URLの読み上げを省略

--- src/aques_talk.py
+++ src/aques_talk.py
@@ -1,15 +1,24 @@
+import re
 import subprocess
 from dataclasses import dataclass
-from typing import Optional
+from typing import Final, Optional
+
+URL_PATTERN: Final[str] = r"https?://[\w/:%#\$&\?\(\)~\.=\+\-]+"
 
 
 @dataclass
 class AquesTalkGenerator:
     aques_talk_path: Optional[str]
 
-    def generate(self, destination_path: str, message: str):
-        message_clean = message.replace("'", "")
+    def generate(self, destination_path: str, message: str) -> None:
+        message_clean = self.preprocess_message(message=message)
         subprocess.run(
             f"{self.aques_talk_path} '{message_clean}' > {destination_path}",
             shell=True,
         )
+
+    def preprocess_message(self, message: str) -> str:
+        if re.match(URL_PATTERN, message):
+            return "URL省略"
+        message = message.replace("'", "")
+        return message
